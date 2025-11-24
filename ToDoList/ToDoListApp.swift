3a9276317c214f19c328a2bfa8d2ca66a1b1c0c1
Artem Kurochkin <ArtemQ98@yandex.ru>
2025-11-24 18:52:23 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Артём Курочкин on 24.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
